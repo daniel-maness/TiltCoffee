4584e0482699027606d226915b361d9007aa9422
Daniel Maness <daniel.maness@gmail.com>
2016-05-31 11:40:23 -0700

    New project with custom requirements set

---
+++ TiltCoffee/TiltCoffeeUITests/TiltCoffeeUITests.swift
@@ -0,0 +1,36 @@
+//
+//  TiltCoffeeUITests.swift
+//  TiltCoffeeUITests
+//
+//  Created by Daniel Maness on 5/31/16.
+//  Copyright © 2016 Devious Logic. All rights reserved.
+//
+
+import XCTest
+
+class TiltCoffeeUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
